ea47bc74d9fdc94813d21d7cbac3d367b80c530a
federicopigozzi <pigozzife@gmail.com>
2019-11-19 10:40:23 +0100

    unknown amount of numbers

--- test/source/dssc/calculator/StringCalculator.java
+++ test/source/dssc/calculator/StringCalculator.java
@@ -1,13 +1,18 @@
 package dssc.calculator;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StringCalculator {
     public static int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
         else if (numbers.contains(",")) {
-            String[] tokens = numbers.split(",");
-            return Integer.valueOf(tokens[0]) + Integer.valueOf(tokens[1]);
+            //String[] tokens = numbers.split(",");
+            Stream<String> tokens = Arrays.stream(numbers.split(","));
+            return tokens.mapToInt(Integer::valueOf).sum();
+            //return Integer.valueOf(tokens[0]) + Integer.valueOf(tokens[1]);
         }
         else {
             return Integer.valueOf(numbers);
--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -3,6 +3,8 @@ package test;
 import dssc.calculator.StringCalculator;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,5 +25,22 @@ public class AddNumbersTest {
     @Test
     void twoNumbers() {
         assertThat(StringCalculator.add("1,2"), is(3));
+    }
+
+    @Test
+    void anyNumbers() {
+        Random random = new Random();
+        int numNumbers = 100;
+        int maxNum = 100;
+        int num = random.nextInt(numNumbers + 1);
+        int currNum = random.nextInt(maxNum + 1);
+        String testString = String.valueOf(currNum);
+        int sum = currNum;
+        for (int i=0; i < num - 1; ++i) {
+            currNum = random.nextInt(maxNum + 1);
+            testString += "," + currNum;
+            sum += currNum;
+        }
+        assertThat(StringCalculator.add(testString), is(sum));
     }
 }
